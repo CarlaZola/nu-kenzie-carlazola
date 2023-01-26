1aa350da2e8164a0ab1791878c2eb5a5ab4df333
carla <carla.zollaa@gmail.com>
2023-01-26 11:11:12 -0300

    fix: semicolon in style

--- src/pages/Dashboard/dashboard.js
+++ src/pages/Dashboard/dashboard.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
-const StyledDashboard = styled.main`
+ const StyledDashboard = styled.main`
     min-height: 100vh;
-    background-color: ${({theme}) => theme.colors.colorGrey4}
+    background-color: ${({theme}) => theme.colors.colorGrey4};
 `
+
 
 export default StyledDashboard
